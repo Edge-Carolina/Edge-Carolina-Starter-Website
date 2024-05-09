7a61f9e6a3c5f3f1e2b314ad0a890fddb654b055
Weston Voglesonger <111527221+tvogle@users.noreply.github.com>
2024-05-09 17:06:33 -0400

    Remove <app-navigation> template from app.component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  template: `
-    <app-navigation></app-navigation>
-    <router-outlet></router-outlet>
-  `,
+  template: ` <router-outlet></router-outlet> `,
 })
 export class AppComponent {
   title = "frontend";
